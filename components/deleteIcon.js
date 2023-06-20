91702ec4899e40fe0adac07262fb7d6da7c75e0e
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-06-20 14:02:25 +0200

    completed

--- components/deleteIcon.js
+++ components/deleteIcon.js
@@ -1,15 +1,13 @@
 const deleteIcon = () => {
-    const i = document.createElement("i");
-    i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
-    i.addEventListener("click", deleteTask);
-    return i;
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+  i.addEventListener('click', deleteTask);
+  return i;
 };
 
 const deleteTask = (event) => {
-    //console.log(event.target.parentElement);
-    const parent = event.target.parentElement;
-    console.log(parent);
-    parent.remove();
+  const parent = event.target.parentElement;
+  parent.remove();
 };
 
 export default deleteIcon;
